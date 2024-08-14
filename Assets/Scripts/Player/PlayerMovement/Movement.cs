92bbbfe82af641c187c4df4a718baaacd43b085d
zafericen <zafericen1907@gmail.com>
2024-08-14 20:45:26 +0300

    some changes to the movement

--- Assets/Scripts/Player/PlayerMovement/Movement.cs
+++ Assets/Scripts/Player/PlayerMovement/Movement.cs
@@ -11,31 +11,32 @@ namespace ProjectZephyr
 
         public void Move()
         {
-            var TargetVector = new Vector3(inputHandler.InputVector.x, 0, inputHandler.InputVector.y);
-            var MovementVector = MoveTowardTarget(TargetVector.normalized);
-            RotateTowardMovementVector(MovementVector);
+            var TargetVector = inputHandler.InputVector.x;
+            //transform.position.x +=TargetVector;
+            //var MovementVector = MoveTowardTarget(TargetVector.normalized);
+            //RotateTowardMovementVector(MovementVector);
         }
 
-        private void RotateTowardMovementVector(Vector3 movementVector)
-        {
-            if (movementVector.magnitude == 0)
-            {
-                return;
-            }
-            var Rotation = Quaternion.LookRotation(movementVector);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Rotation, rotationSpeed);
-        }
+        //private void RotateTowardMovementVector(Vector3 movementVector)
+        //{
+        //    if (movementVector.magnitude == 0)
+        //    {
+        //        return;
+        //    }
+        //    var Rotation = Quaternion.LookRotation(movementVector);
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Rotation, rotationSpeed);
+        //}
 
-        private Vector3 MoveTowardTarget(Vector3 TargetVector)
-        {
-            var Speed = moveSpeed * Time.deltaTime;
+        //private Vector3 MoveTowardTarget(Vector3 TargetVector)
+        //{
+        //    var Speed = moveSpeed * Time.deltaTime;
 
-            TargetVector = Quaternion.Euler(0, cam.gameObject.transform.eulerAngles.y, 0) * TargetVector;
-            var TargetPosition = transform.position + TargetVector * Speed;
-            transform.position = TargetPosition;
+        //    TargetVector = Quaternion.Euler(0, cam.gameObject.transform.eulerAngles.y, 0) * TargetVector;
+        //    var TargetPosition = transform.position + TargetVector * Speed;
+        //    transform.position = TargetPosition;
 
-            return TargetVector;
-        }
+        //    return TargetVector;
+        //}
         
     }
 
