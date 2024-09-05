77afe74cebe1633a04b03c7fb4fde02fe1ed3ac2
zafericen <zafericen1907@gmail.com>
2024-09-05 16:04:36 +0300

    .

--- Assets/Scripts/Combat/AbilitySystem/AbilitySlot.cs
+++ Assets/Scripts/Combat/AbilitySystem/AbilitySlot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbilitySlot : MonoSingleton<AbilitySlot>
+public class AbilitySlot: MonoSingleton<AbilitySlot>
 {
     public List<GameObject> abilityHolders;
 
